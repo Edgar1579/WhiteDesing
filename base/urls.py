06322eb36106aa9fbbd9f0f136155d34471b3b0b
Edgar1579 <es535965@gmail.com>
2023-10-04 18:41:15 -0500

    creando templates

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from base.views import bienvenidos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', bienvenidos, name="inicio"),
 ]
