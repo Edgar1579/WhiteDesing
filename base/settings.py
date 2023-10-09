847a6f19c2460f8ca40260d6db0ac4abad0c22c9
Edgar1579 <es535965@gmail.com>
2023-10-09 18:47:50 -0500

    ok

--- base/settings.py
+++ base/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5', # pip pip install boostrap-v5
+    'bootstrap5', # pip install boostrap-v5
 ]
 
 MIDDLEWARE = [
