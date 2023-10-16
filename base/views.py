47ccc2cdea74a0308e93b55ac0409c26d9bc8091
Edgar1579 <es535965@gmail.com>
2023-10-16 17:31:02 -0500

    añadi logo al navbar

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def bienvenidos(request):
+    titulo= "inicio"
     context={
-        
+        "titulo": titulo,
     }
     return render(request, 'index.html', context)
